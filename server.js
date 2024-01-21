8b038f3206c11ecd8b6019da58b95791fbb15a8b
rishi-2811 <mishrarishi2911@gmail.com>
2024-01-21 23:52:13 +0530

    added user schema

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
 const express=require('express')
 const app=express()
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const bcrypt=require('bcrypt')
+const { error } = require('console')
+const User=require('./user')
+dotenv.config()
+const saltRounds=10
 
+mongoose.connect(process.env.URL).then(console.log('Mongodb is connected'))
+
+app.use(express.json())
+
+app.post('/signup',async(req,res)=>{
+    try {
+        const user=await User.find({$or:[
+            {username: req.body.username},
+            {email: req.body.email}]})
+        if (user){
+            res.send('User already exists').status(401)
+        }
+       await bcrypt.hash(req.body.password,saltRounds,async(hash,err)=>{
+           if(err){
+            console.log(err)
+            res.status(500)
+           }
+           const newuser= await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash
+        })
+        res.send(newuser)
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+    }
+})
 
 app.listen(3000,()=>{
     console.log("server is running")
---
+++ user.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose')
+
+const userschema=new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    hashedpassword:{
+        type: String,
+        required: true,
+        minLength: 8,
+    }
+})
+
+const user=mongoose.model('user',userschema)
+
+module.exports=user
