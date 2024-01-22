a21e35dce8a089523c2453b610c9fb72e044413b
rishi-2811 <mishrarishi2911@gmail.com>
2024-01-22 11:42:57 +0530

    added jwt and bcrypt successfully

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const dotenv=require('dotenv')
 const bcrypt=require('bcrypt')
 const { error } = require('console')
 const User=require('./user')
+const jwt=require('jsonwebtoken')
 dotenv.config()
 const saltRounds=10
+const seckey=process.env.key
 
 mongoose.connect(process.env.URL).then(console.log('Mongodb is connected'))
 
@@ -14,24 +16,56 @@ app.use(express.json())
 
 app.post('/signup',async(req,res)=>{
     try {
-        const user=await User.find({$or:[
+        const user=await User.findOne({$or:[
             {username: req.body.username},
             {email: req.body.email}]})
         if (user){
-            res.send('User already exists').status(401)
+           return res.send('User already exists').status(401)
         }
-       await bcrypt.hash(req.body.password,saltRounds,async(hash,err)=>{
-           if(err){
-            console.log(err)
-            res.status(500)
-           }
-           const newuser= await User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: hash
+        bcrypt.genSalt(saltRounds,(err,salt)=>{
+            if(err)
+            return res.status(500)
+            bcrypt.hash(req.body.hashedpassword,salt,async(err,hash)=>{
+                    if(err){
+                     console.log(err)
+                     return res.status(500)
+                    }
+                    const newuser= await User.create({
+                     username: req.body.username,
+                     email: req.body.email,
+                     hashedpassword: hash
+                 })
+                 return res.redirect('./login')
+            })
         })
-        res.send(newuser)
-        })
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+    }
+})
+
+app.post('/login',async(req,res)=>{
+    try {
+        const user=await User.findOne({$or:[
+            {username: req.body.username},
+            {email: req.body.username}]})
+        if (!user){
+           return res.send('New User').status(401)
+        }
+        bcrypt.compare(req.body.hashedpassword,user.hashedpassword,(err,result)=>{
+            if(err){
+                console.log(err)
+              return res.status(500)
+            }
+            if(result){
+                const payload = Object.assign({}, user)
+                const token=jwt.sign(payload,seckey,{expiresIn:'1h'})
+               return res.send(token)
+            }
+            return res.send('incorrect password')
+        }) 
+       
+    
     } catch (error) {
         console.log(error)
         res.status(500)
