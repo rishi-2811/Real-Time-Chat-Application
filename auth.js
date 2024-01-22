214d69423a0f5c326b55c74179facb36944001f7
rishi-2811 <mishrarishi2911@gmail.com>
2024-01-22 16:55:08 +0530

    added cookie and authentication

---
+++ auth.js
@@ -0,0 +1,22 @@
+const jwt=require('jsonwebtoken')
+const cookieParser=require('cookie-parser')
+const dotenv=require('dotenv')
+dotenv.config()
+const seckey=process.env.key
+
+
+const auth=(req,res,next)=>{
+    const token=req.cookies.access_token
+    if(!token)
+    return res.status(403)
+    try {
+        const data=jwt.verify(token,seckey)
+        req.body=data
+        return next()
+    } catch (error) {
+        console.error(error)
+        return res.status(500)
+    }
+}
+
+module.exports=auth
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const bcrypt=require('bcrypt')
 const { error } = require('console')
 const User=require('./user')
 const jwt=require('jsonwebtoken')
+const cookieParser=require('cookie-parser')
+const auth=require('./auth')
 dotenv.config()
 const saltRounds=10
 const seckey=process.env.key
@@ -13,6 +15,7 @@ const seckey=process.env.key
 mongoose.connect(process.env.URL).then(console.log('Mongodb is connected'))
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.post('/signup',async(req,res)=>{
     try {
@@ -58,9 +61,13 @@ app.post('/login',async(req,res)=>{
               return res.status(500)
             }
             if(result){
-                const payload = Object.assign({}, user)
-                const token=jwt.sign(payload,seckey,{expiresIn:'1h'})
-               return res.send(token)
+                const token=jwt.sign({user},seckey,{expiresIn:'1h'})
+               return res.cookie("access_token",token,{
+                httpOnly:true,
+                secure: process.env.NODE_ENV === "production",
+               })
+               .status(200)
+               .json({message:"successfully logged in"})
             }
             return res.send('incorrect password')
         }) 
@@ -70,6 +77,14 @@ app.post('/login',async(req,res)=>{
         console.log(error)
         res.status(500)
     }
+})
+
+app.get('/data',auth,(req,res)=>{
+    return res.send(req.body)
+})
+
+app.get('/logout',auth,(req,res)=>{
+    return res.clearCookie("access_token").json({message:"looged out successfully"})
 })
 
 app.listen(3000,()=>{
